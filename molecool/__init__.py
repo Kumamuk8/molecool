06a3d968026c58d573f37de77be8d7b34dd7ef9e
Ruiyang Li <rli8@nd.edu>
2020-12-15 11:46:50 -0500

    Modify import statement in the init file

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,7 @@ Python package for analyzing and visualizing xyz files
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas
 
 # Handle versioneer
 from ._version import get_versions
